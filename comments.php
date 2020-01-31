8aa0dfeb02c3f1a1a8b2fd36b0a5cbbb8cc6a8b3
CHA2206625 <60496929+CHA2206625@users.noreply.github.com>
2020-01-31 15:37:43 -0700

    Add files via upload

---
+++ comments.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html  lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Hello World</title>
+  <style type="text/css">
+  .bold {
+	  font-weight: bolder;
+  }
+  </style>
+  </head>
+  <body>
+<p>The following was created by PHP:
+<br>
+<?php
+/*
+ * Filename: Comments.php
+ * Created by: Chad Moklestad
+ *
+ */
+ // Another way to comment is two slashes
+ # I hear you can use the number sign as well
+ /* if I have a lot to say, 
+ I might need to use the multi-line comments */
+ 
+ print "<span class=\"bold\">Hello, World!</span>";
+ 
+ 
+ ?>
+ <!-- the comments for HTML are different as you can see
+ -->
+</p>
+
+
+
+  </body>
+  </html>
