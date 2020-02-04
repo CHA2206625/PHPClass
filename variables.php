9fe94eada4e59171ccd9d69b6a3bbb4c83a23290
CHA2206625 <60496929+CHA2206625@users.noreply.github.com>
2020-02-03 21:05:21 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,28 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>variables.php</title>
+</head>
+<body>
+
+<?php
+// Script 2.3 - variables.php
+
+//name
+$firstName = 'Chad';
+$lastName = 'Moklestad';
+//phone
+$phone = '555-555-5555';
+//Address
+$street = '123 Montebello Ave';
+$city = 'Phoenix';
+$state = 'AZ';
+$zip = '12345';
+
+print "<h2>Ship to:</h2><h3>$firstName $lastName<br/> $street<br/>$city, $state $zip</h3> <p><em>Contact: <strong>$phone</strong> upon reciept</em></p>"
+
+?>
+</body>
+</html>
