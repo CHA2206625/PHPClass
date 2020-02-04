8b76edc12d983b48172b47aacf065138a3a400d6
CHA2206625 <60496929+CHA2206625@users.noreply.github.com>
2020-02-03 20:02:31 -0700

    Add files via upload

---
+++ predefined.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Predifined Variables</title>
+</head>
+<body>
+<pre>
+
+<?php //Script 2.1 - predefined.php
+		// Chad Moklestad
+		print_r($_SERVER);
+?>
+</pre>
+
+
+</body>
+</html>
